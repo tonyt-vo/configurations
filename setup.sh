8171731843a4f486fde2da225b03d893ff758d7d
Tony Vo <tonyt.vo22@gmail.com>
2018-11-21 17:26:53 -0800

    beginning configuration setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,45 @@
-echo 'Changing git credentials...'
-git config --global user.email "tonyt.vo22@gmail.com"
-git config --global user.name "Tony Vo"
+#!/bin/bash
+# Author: Tony Vo
+# Description: Quick set up script
+
+main() {
+    echo 'This script changes the following:'
+    echo -e '\tInstalls ZSH as default shell'
+    echo -e '\t git credentials'
+    echo -e '\t.vimrc files in home directory'
+    echo -e '\t.tmux.conf files in home directory'
+    echo -e '\t.bashrc files in home directory'
+    read -r -p 'Are you sure you want to continue? [Y/N]' response
+
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+      change_git_credentials
+      move_dot_files 
+    else
+      echo 'Exiting... No actions completed.' 
+    fi
+}
+
+
+change_git_credentials() {
+    echo 'Changing git credentials...'
+    git config --global user.email "tonyt.vo22@gmail.com"
+    git config --global user.name "Tony Vo"
+    echo 'Git credentials changed successfully'
+}
+
+move_dot_files() {
+    echo 'Overriding .vimrc...'
+    mv .vimrc ~/
+
+    echo 'Overriding .tmux.conf...'
+    mv .tmux.conf ~/
+
+    echo 'Overriding .bashrc...'
+    mv .bashrc ~/
+
+    echo 'Overriding .zsh...'
+    mv .zsh ~/
+}
+
+main
