e6119d7abf94e8bebaf26177a3b6b16880c791c9
Tony Vo <tonyt.vo22@gmail.com>
2020-02-02 11:29:26 -0800

    add commands for setting up windows

---
+++ setup_windows.ps1
@@ -0,0 +1,17 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install vscode -y
+choco install node -y
+choco install python -y
+choco install ruby -y
+choco install git -y
+choco install autohotkey y
+choco install vim -y
+choco install intellijidea-community -y
+choco install microsoft-windows-terminal -y
+choco install googlechrome -y
+
+New-Item -Path "c:\" -Name "Workspace" -ItemType "directory"
+cd C:\Workspace
+git clone https://github.com/tonyt-vo/configurations.git
+cd C:\Worspace\configurations
+./setup.sh
